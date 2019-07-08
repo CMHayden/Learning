4da150c421909956eb47a34f245942fc8a8748be
CMHayden <haydencallum4@gmail.com>
2019-07-08 15:31:24 +0200

    creating a snippit to expand an svg image on click

---
+++ Scene.js/snippits/expandOnClick.js
@@ -0,0 +1,38 @@
+/*
+*   To make an element (#cta in this case) change size when clicked.
+*   The numbered objects, represent the time in seconds from when clicked. 
+*   The property for transformation can be done in this way or the same way as fadeIn.js
+*
+*   To modify the size, change scale. Speed, change the numbered objects.
+*/
+
+
+var cta = document.getElementById("cta");
+
+var scene2 = new Scene({
+    "#cta" : {
+        0: {
+            transform: {
+                scale: 1
+            }
+        },
+        .3: {
+            transform: {
+                scale: 1.5
+            }
+        },
+        .5: {
+            transform: {
+                scale: 1
+            }
+        }
+    }
+},
+{
+    easing: Scene.EASE_IN_OUT,
+    selector: true
+})
+
+cta.addEventListener('click', () => {
+    scene2.playCSS()
+})
