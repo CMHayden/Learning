9cf591ff8b55289c3ad63785413e01fa05806ed3
CMHayden <haydencallum4@gmail.com>
2019-07-09 15:33:36 +0200

    update communication controller

--- Laravel/communicator/app/Http/Controllers/CommunicationController.php
+++ Laravel/communicator/app/Http/Controllers/CommunicationController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Twilio\Rest\Client;
+
 
 class CommunicationController extends Controller
 {
-    //
+    public function index(Request $request) {
+
+        $requestJson = json_decode($request->getContent(), true);
+
+        //$this->sendEmail($requestJson);
+
+        $this->sendSMS($requestJson);
+
+    }
+
+    public function sendSMS($requestJson) {
+        //
+    }
+
+    public function sendEmail($requestJson) {
+        //
+    }
+
 }
