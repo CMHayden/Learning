a2355a5d0172718ac5abeb3635398628def70dee
Callum Hayden <haydencallum4@gmail.com>
2020-08-02 20:22:50 +0100

    add babel loader for all js files in webpack config

--- Webpack/webpacktut/webpack.config.js
+++ Webpack/webpacktut/webpack.config.js
@@ -3,6 +3,13 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
     module: {
         rules: [
+            { // use babel loader for all non-node module JS files
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
             { // use html loader for all html files and minimize the files.
                 test: /\.html$/,
                 use: [
